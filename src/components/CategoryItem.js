9938baad6c947b2748fc898716447590394b5c70
JB Tellez <jb.tellez@gmail.com>
2018-08-11 23:04:40 -0700

    solves

---
+++ src/components/CategoryItem.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { removeCategory, updateCategory } from '../store/category'; 
+import CategoryForm from './CategoryForm';
+
+
+class CategoryItem extends Component {
+
+  state = {
+    editing: false
+  }
+
+  onDelete = () => {
+    this.props.removeCategory(this.props.category);
+  }
+
+  onEdit = () => {
+    this.setState({ editing: true })
+  }
+
+  onUpdate = (category) => {
+    this.props.updateCategory(category);
+  }
+
+  render() {
+    return (
+      <li onClick={this.onEdit}>
+        <p>
+          {this.props.category.name}
+        </p>
+        <p>
+        ${this.props.category.budget}
+        </p>
+        <p>
+          {new Date(this.props.category.timestamp).toLocaleDateString()}
+        </p>
+        <p>
+          <button onClick={this.onDelete}>x</button>
+        </p>
+
+        {this.state.editing && <CategoryForm onComplete={this.onUpdate} buttonText="Edit" category={this.props.category} />}
+      </li>
+    );
+  }
+}
+
+
+
+const mapDispatchToProps = (dispatch) => ({
+  removeCategory: category => dispatch(removeCategory(category)),
+  updateCategory: category => dispatch(updateCategory(category)),
+});
+
+CategoryItem.propTypes = {
+  removeCategory: PropTypes.func,
+  updateCategory: PropTypes.func,
+  category: PropTypes.object,
+}
+
+export default connect(null, mapDispatchToProps)(CategoryItem);
+
+
---
+++ src/components/CategoryList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import CategoryItem from './CategoryItem';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const CategoryList = (props) => {
+  return (
+    <ul>
+      {props.categories.map(cat => <CategoryItem key={cat.id} category={cat} />)}
+    </ul>
+  );
+};
+
+const mapStateToProps = ({ categories }) => ({ categories });
+
+export default connect(mapStateToProps, null)(CategoryList);
+
+CategoryList.propTypes = {
+  categories: PropTypes.arrayOf(Object)
+}
